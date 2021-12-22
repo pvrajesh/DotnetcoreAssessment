e2c32190861cddbf64288540e6907d8df4615e6a
pvrajesh <rajeshpv.ece@gmail.com>
2021-12-22 08:55:22 +0530

    Update EventController.cs
    removing deafault comment

--- Fixture/Controllers/EventController.cs
+++ Fixture/Controllers/EventController.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text.Json;
 using statuscodes= Microsoft.AspNetCore.Http.StatusCodes;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
 
 namespace Fixture.Controllers
 {
